966eb93584896529f41a1462cef0c11dd9a78df3
Ariel Jr Mimura <avmimurajr@gmail.com>
2023-10-23 10:47:53 +0800

    Questions are now working

--- wwwroot/questionProvider.js
+++ wwwroot/questionProvider.js
@@ -1,4 +1,4 @@
-export function getQuestions(difficulty, category) {
+function getQuestions(difficulty, category) {
     return fetch('wwwroot/exam.json')
         .then((response) => response.json())
         .then((data) => {
@@ -7,6 +7,12 @@ export function getQuestions(difficulty, category) {
                 return question.Difficulty === difficulty && question.Category === category;
             });
 
+            // Shuffle the filtered questions array using Fisher-Yates shuffle algorithm
+            for (let i = filteredQuestions.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [filteredQuestions[i], filteredQuestions[j]] = [filteredQuestions[j], filteredQuestions[i]];
+            }
+            
             const uniqueQuestions = new Set(); // Use a Set to ensure uniqueness
 
             // Populate the Set with unique questions
@@ -26,4 +32,76 @@ export function getQuestions(difficulty, category) {
             console.error('Error fetching data:', error);
             throw error;
         });
+}
+
+// Call this function to start displaying questions
+export function startQuiz() {
+    const difficulty = sessionStorage.getItem('difficulty');
+    const category = sessionStorage.getItem('category');
+    getQuestions(difficulty, category)
+        .then((selectedQuestions) => {
+            populateWithQuestion(selectedQuestions, 0);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+let currentScore = 0;
+function populateWithQuestion(selectedQuestions, questionCount) {
+    const questionContainer = document.getElementById('questionContainer');
+
+    if (questionCount < selectedQuestions.length) {
+        // Display the question
+        const question = selectedQuestions[questionCount];
+
+        const questionDiv = document.createElement('div');
+        questionDiv.className = 'd-flex flex-column';
+
+        const questionText = document.createElement('p');
+        questionText.innerHTML = `<strong>Question:</strong> ${question.Question}`;
+
+        const answerInput = document.createElement('input');
+        answerInput.type = 'text';
+        answerInput.placeholder = question.QuestionType == 'TrueFalse' ? 'True or False' : 'Your answer...';
+        answerInput.className = 'form-control border-dark';
+
+        const verificationText = document.createElement('span');
+        verificationText.className = ('small text-right')
+
+        const submitButton = document.createElement('button');
+        submitButton.textContent = 'Submit';
+        submitButton.className = 'btn btn-sm btn-outline-dark mt-3 col-md-3 justify-content-end';
+
+        submitButton.addEventListener('click', () => {
+            const userAnswer = answerInput.value;
+            if (userAnswer.toLowerCase() === question.Answer.toLowerCase()) {
+                currentScore++;
+                answerInput.className = 'form-control border-success'
+                verificationText.innerHTML = `Correct! Current Score: ${currentScore}`;
+            } else {
+                answerInput.className = 'form-control border-danger'
+                verificationText.innerHTML = `Correct Answer: ${question.Answer}`;
+            }
+
+            setTimeout(function () {
+                // Move to the next question
+                questionDiv.remove();
+                populateWithQuestion(selectedQuestions, questionCount + 1);
+            }, 2000)
+        });
+
+        questionDiv.appendChild(questionText);
+        questionDiv.appendChild(answerInput);
+        questionDiv.appendChild(verificationText);
+        questionDiv.appendChild(submitButton);
+
+        questionContainer.appendChild(questionDiv);
+    } else {
+        const resultText = document.createElement('p');
+        resultText.innerHTML =
+            `<strong>Quiz Complete</strong>` +
+            `<p class='mt-2 small text-center'>Total Score: ${currentScore}</p>`;
+        questionContainer.appendChild(resultText);
+    }
 }
--- wwwroot/quizapp.js
+++ wwwroot/quizapp.js
@@ -1,4 +1,4 @@
-import { getQuestions } from "./questionProvider.js";
+import { startQuiz } from "./questionProvider.js";
 
 // Function to handle the submission of the name and showing the game controls
 document.getElementById('submitName').addEventListener('click', function () {
@@ -39,58 +39,6 @@ categoryButtons.forEach(button => {
     startQuiz();
   });
 });
-
-// Call this function to start displaying questions
-function startQuiz() {
-  const difficulty = sessionStorage.getItem('difficulty');
-  const category = sessionStorage.getItem('category');
-  getQuestions(difficulty, category)
-    .then((selectedQuestions) => {
-      populateWithQuestion(selectedQuestions, 0);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-}
-
-function populateWithQuestion(selectedQuestions, questionCount) {
-  const questionContainer = document.getElementById('questionContainer');
-
-  if (questionCount < selectedQuestions.length) {
-    // Display the question
-    const question = selectedQuestions[questionCount];
-    
-    const questionDiv = document.createElement('div');
-    questionDiv.className = 'd-flex flex-column';
-    
-    const questionText = document.createElement('p');
-    questionText.innerHTML = `<strong>Question:</strong> ${question.Question}`;
-    
-    const answerInput = document.createElement('input');
-    answerInput.type = 'text';
-    answerInput.placeholder = 'Your answer...';
-    
-    const submitButton = document.createElement('button');
-    submitButton.textContent = 'Submit';
-    submitButton.className ='btn btn-sm btn-outline-dark mt-3 col-md-3 justify-content-end';
-
-    submitButton.addEventListener('click', () => {
-      const userAnswer = answerInput.value;
-      console.log('User Answer:', userAnswer);
-      
-      // Move to the next question
-      questionDiv.remove();
-      populateWithQuestion(selectedQuestions, questionCount + 1);
-    });
-
-    questionDiv.appendChild(questionText);
-    questionDiv.appendChild(answerInput);
-    questionDiv.appendChild(submitButton);
-    questionContainer.appendChild(questionDiv);
-  } else {
-    console.log("All questions displayed.");
-  }
-}
 
 // Deletes the currently saved user session
 document.getElementById('quitGameBtn').addEventListener('click', function () {
